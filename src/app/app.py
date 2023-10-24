f60ff5ceca986e086db175f1b4d147e65d5819f0
Ria Gupta <ria.gupta@boeing.com>
2023-10-24 10:32:14 -0700

    writing-to-faiss

--- src/app/app.py
+++ src/app/app.py
@@ -1,52 +1,103 @@
-import torch
-torch.device('cpu')
-
-# Imports and Initializations for Chainlit
+import pandas as pd
+import json
+import re, os
+from bs4 import BeautifulSoup
+import unicodedata
+import requests
 import chainlit as cl
 from dotenv import load_dotenv
-
-# Imports and Initializations for Haystack
+import torch
+torch.device('cpu')
 from haystack.document_stores import InMemoryDocumentStore
+#import datasets
 from datasets import load_dataset
 from haystack.nodes import BM25Retriever, PromptTemplate, AnswerParser, PromptNode
-import os
 from haystack.pipelines import Pipeline
+from haystack.document_stores import FAISSDocumentStore
+from haystack.nodes import EmbeddingRetriever, PreProcessor, TextConverter
 
-# Load environment variables (if any)
-load_dotenv("../.env")
-load_dotenv()
-MY_API_KEY = ""
+path = 'countrytravelinfo.json'
+with open(path) as f:
+    data = json.loads(f.read())
 
-# Initialize Haystack's QA system
-document_store = InMemoryDocumentStore(use_bm25=True)
-dataset = load_dataset("bilgeyucel/seven-wonders", split="train")
-document_store.write_documents(dataset)
+sample_data = data[10]['entry_exit_requirements']
+cleaned_soup = BeautifulSoup(sample_data, "html.parser" )
+soup_text = cleaned_soup.get_text(separator = ' ', strip = True)
+clean_text_again = unicodedata.normalize("NFKD",soup_text)
 
+def html_cleaning(json_file):
+    """ Remove html tags from text
+            input: json data
+        - These fileds will be cleaned:
+            * travel_transportation
+            * health
+            * local_laws_and_special_circumstances
+            * safety_and_security
+            * entry_exit_requirements
+            * destination_description
+            * travel_embassyAndConsulate
+            """
+    feature_list = [ 'travel_transportation',
+                     'health',
+                     'local_laws_and_special_circumstances',
+                     'safety_and_security',
+                     'entry_exit_requirements',
+                     'destination_description',
+                     'travel_embassyAndConsulate']
+    
+    for idx, entry in enumerate(json_file):
+        for feat in feature_list:
+            raw_text = entry[feat]
+            cleaned = BeautifulSoup(raw_text, "html.parser" )
+            cleaned_text = cleaned.get_text(separator = ' ', strip = True)
+            final_text = unicodedata.normalize("NFKD",cleaned_text)
+            json_file[idx][feat] = final_text
+            
+    return json_file
 
-rag_prompt = PromptTemplate(
-    prompt="""Synthesize a brief answer from the following text for the given question.
-                             Provide a clear and concise response that summarizes the key points and information presented in the text.
-                             Your answer should be in your own words and be no longer than 50 words.
-                             \n\n Related text: {join(documents)} \n\n Question: {query} \n\n Answer:""",
-    output_parser=AnswerParser(),
+html_cleaned_dataset = html_cleaning(data)
+df = pd.DataFrame(html_cleaned_dataset)
+document_store = FAISSDocumentStore()
+document_store.delete_documents()
+indexing_pipeline4 = Pipeline()
+text_converter = TextConverter()
+
+indexing_pipeline4.add_node(component=text_converter, name="TextConverter", inputs=["File"])
+preprocessor = PreProcessor(
+    clean_empty_lines=True,
+    clean_whitespace=False,
+    clean_header_footer=True,
+    split_by="word",
+    split_length=500,
+    split_respect_sentence_boundary=True,
 )
-
-# Set up nodes
-retriever = BM25Retriever(document_store=document_store, top_k=2)
-pn = PromptNode("gpt-3.5-turbo", 
-                api_key=MY_API_KEY, 
-                model_kwargs={"stream":False},
-                default_prompt_template=rag_prompt)
-
-# Set up pipeline
-pipe = Pipeline()
-pipe.add_node(component=retriever, name="retriever", inputs=["Query"])
-pipe.add_node(component=pn, name="prompt_node", inputs=["retriever"])
-
+indexing_pipeline4.add_node(component=preprocessor, name="PreProcessor", inputs=["TextConverter"])
+retriever = EmbeddingRetriever(document_store=document_store, embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
+indexing_pipeline4.add_node(component=retriever, name="EmbeddingRetriever", inputs=["PreProcessor"])
+indexing_pipeline4.add_node(component=document_store, name="document_store", inputs=['EmbeddingRetriever'])
+doc_dir = '/Users/ria.gupta/hack-chat/doc'
+files_to_index = [doc_dir+ "/" + f for f in os.listdir(doc_dir)]
+indexing_pipeline4.run_batch(file_paths=files_to_index)
+prompt_template = PromptTemplate(prompt = """"Answer the following query based on the provided context. If the context does
+                                                not include an answer, reply with 'The data does not contain information related to the question'.\n
+                                                Query: {query}\n
+                                                Documents: {join(documents)}
+                                                Answer: 
+                                            """,
+                                            output_parser=AnswerParser())
+openai_key = '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'
+prompt_node = PromptNode(model_name_or_path = "gpt-4",
+                            api_key = openai_key,
+                            default_prompt_template = prompt_template,
+                            max_length = 500,
+                            model_kwargs={"stream":True})
+query_pipeline = Pipeline()
+query_pipeline.add_node(component=retriever, name="Retriever", inputs=["Query"])
+query_pipeline.add_node(component=prompt_node, name="PromptNode", inputs=["Retriever"])
 @cl.on_message
 async def main(message: str):
     # Use the pipeline to get a response
-    output = pipe.run(query=message)
+    output = query_pipeline.run(query=message)
 
     # Create a Chainlit message with the response
     response = output['answers'][0].answer
